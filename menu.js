259eda99fa29eee9449ec204598fbf93c419c0dc
unknown <k00242869@student.lit.ie>
2020-11-19 17:54:04 +0000

    adding pages

---
+++ menu.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+( function( $ ) {
+
+	$( '.dropdown-toggle' ).click( function( e ) {
+		var _this = $( this );
+		e.preventDefault();
+		_this.toggleClass( 'toggle-on' );
+		_this.parent().next( '.sub-menu' ).toggleClass( 'toggled-on' );
+		_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+		_this.html( _this.html() === '<span class="screen-reader-text">Expand child menu</span>' ? '\
+    <span class="screen-reader-text">Collapse child menu</span>' : '<span class="screen-reader-text">\n\
+Expand child menu</span>' );
+	} );
+
+})( jQuery );
